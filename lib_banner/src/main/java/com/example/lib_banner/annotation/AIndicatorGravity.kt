d78749fed6fba598fa995e3abefffd81d45e5bae
李行宇 <lixingyu@hosecloud.com>
2023-11-19 18:13:54 +0800

    提交配置文件

---
+++ lib_banner/src/main/java/com/example/lib_banner/annotation/AIndicatorGravity.kt
@@ -0,0 +1,15 @@
+package com.sum.banner.annotation
+
+import android.annotation.SuppressLint
+import androidx.annotation.IntDef
+import com.sum.banner.mode.IndicatorGravity
+import java.lang.annotation.ElementType
+import java.lang.annotation.Target
+
+/**
+ * 指示器位置
+ */
+@IntDef(IndicatorGravity.CENTER, IndicatorGravity.START, IndicatorGravity.END)
+@kotlin.annotation.Retention(AnnotationRetention.SOURCE)
+@Target(ElementType.PARAMETER)
+annotation class AIndicatorGravity()
